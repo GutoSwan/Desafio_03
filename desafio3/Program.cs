f40a1b6099a25fd53684a05605c703c1ed68878e
GutoSwan <gubuenosa@gmail.com>
2022-04-07 20:13:26 -0300

    Desafio3 concluido

---
+++ desafio3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace desafio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<aluno>alunos = new List<aluno>();
+
+            Console.WriteLine("digite a quantidade de alunos");
+            int quantidadeAlunos = Convert.ToInt32(Console.ReadLine());
+
+            for (var count = 0; count < quantidadeAlunos; count++)
+            {
+                aluno aluninho = new aluno();
+                Console.WriteLine("digite o nome do aluno");
+                aluninho.nome = Console.ReadLine();
+                Console.WriteLine("digite a nota do aluno");
+                aluninho.nota = Convert.ToInt32(Console.ReadLine());
+                alunos.Add(aluninho);
+            }
+            var nomeAlunoMaiorNota = alunos.Select(x => x.nota).Max();
+            Console.WriteLine($"aluno com a maior nota é: {alunos.Where(x => x.nota == nomeAlunoMaiorNota).Select(x => x.nome).FirstOrDefault()} ");
+
+        }
+    }
+}
---
+++ desafio3/aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio3
+{
+    public class aluno
+    {
+        public string nome { get; set; }
+        public int nota { get; set; }
+     
+    }
+}
